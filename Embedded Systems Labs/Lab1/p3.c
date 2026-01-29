5609a0cfa9939d13c7f3270526a2c61902442fae
dlove13 <dlove13@ku.edu>
2026-01-28 20:54:27 -0600

    Completed Lab1
    p2, p3, p8

--- Embedded Systems Labs/Lab1/p3.c
+++ Embedded Systems Labs/Lab1/p3.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 
 //Declare function
-int findPrimes();
+int isPrime(); //checks if an individual number is prime
 
 //Define Function
-int findPrimes(int n) {
-    if (n <= 1) {
-        return 0;
-    }
-
-    for (int i = 2; i <= sqrt(n); i++) {
-        if (n % i != 0) {
-            return 0;
+int isPrime(int n) {
+    for (int i = 2; i * i <= n; i++) { //i*i <=n signifies the sqrt(n)
+        if (n % i == 0) {               //function checks for primes until the sqrt(n)
+            return 0;                   //returns false if current value is not prime
         }
     }
-    return 1;
+    return 1;                           //returns true if current value is prime
 }
 
 int main() {
 
-    int userInput;
+    int userInput;  
 
-    printf("Enter a value: ");
+    printf("Enter a value: "); //prompts user to input a single value
     scanf("%d", &userInput);
 
-    findPrimes(userInput);
+    for (int i = 2; i <= userInput; i++){
+        if (isPrime(i)) {               //starting from 2, loop iterates over every value and checks
+            printf("%d ", i);           //if it is prime. if it is prime, then it prints the value to the console
+        }
+    }
 
-    
     return 0;
 }
