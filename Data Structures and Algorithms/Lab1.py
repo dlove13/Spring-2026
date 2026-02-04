5d354d0053c370916e17c14fbe26cc0301412849
dlove13 <dlove13@ku.edu>
2026-02-04 13:07:29 -0600

    lab 1 Data and Assignent 2 Paradigms
    Completed lab1 and started Assignment 2 in programming paradigms

--- Data Structures and Algorithms/Lab1.py
+++ Data Structures and Algorithms/Lab1.py
@@ -1,3 +1,11 @@
+# Author: Davina Love
+# Date: 02-04-2026
+# EECS 330
+
+# AI Disclaimer: Copilot and Gemini were utilized in the debugging of this document,
+#     specifically to prevent unncessary recursion calls that were causing infinite loops
+#     and crashing the program.
+
 #Insertion Sort
 
 def insertion_sort(arr):
@@ -19,53 +27,51 @@ def merge(left, right):
 
     i = j = 0 #initialize iterators
 
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            merged.append(left[i])
+    while i < len(left) and j < len(right):     #compares values and determines which one is lesser than
+        if left[i] <= right[j]:                 #then appends to the merged list
+            merged.append(left[i])              
             i += 1
         else:
             merged.append(right[j])
             j += 1
 
-    merged.extend(left[i:])
-    merged.extend(right[j:])
+    merged.extend(left[i:])                     #safeguard to make sure all elements have been
+    merged.extend(right[j:])                    #accounted for
 
-    return merged
+    return merged                               #output sorted halves
 
 def merge_sort(arr):
 
-    if len(arr) <= 1:
+    if len(arr) <= 1:   #base case once the list is already sorted
         return arr
     
     mid = len(arr) // 2 #find the mid point of the array
-    left_half = merge_sort(arr[:mid])
+    left_half = merge_sort(arr[:mid])    #recursively call the merge_sort to keep dividing the array
+                                            #in half 
     right_half = merge_sort(arr[mid:])
 
-    left_sort = merge_sort(left_half)
-    right_sort = merge_sort(right_half)
+    return merge(left_half, right_half)  #output fully sorted list
 
-    return merge(left_sort, right_sort)
-
-#Insertion Sort Test Cases
+# #Insertion Sort Test Cases
 test_1 = [5, 2, 4, 6, 1, 3]
 test_2 = [6, 7, 81, -96, 77, 101, -1000, 58, 66, 6.5, 13]
 
-#Merge Sort Test Cases
+# #Merge Sort Test Cases
 test_3 = [12, 3, 7, 9, 14, 6, 11, 2]
 test_4 = [4, 1, 2, 3, 0, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-#Run Insertion Sort
+# #Run Insertion Sort
 insertion_sort(test_1)
 insertion_sort(test_2)
 
 #Run Merge Sort
-merge_sort(test_3)
-merge_sort(test_4)
+test_3 = merge_sort(test_3)
+test_4 = merge_sort(test_4)
 
-#Print results to console
-print("Test 1 sorted: ", test_1)
-print("Test 2 sorted: ", test_2)
+# #Print results to console
+print(f"Test 1 sorted: {test_1}")
+print(f"Test 2 sorted: {test_2}")
 
-print("Test 3 sorted: ", test_3)
-print("Test 4 sorted: ", test_4)
+print(f"Test 3 sorted: {test_3}")
+print(f"Test 4 sorted: {test_4}")
 
