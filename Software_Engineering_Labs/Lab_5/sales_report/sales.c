ee368fc469d229889fb448e05a7f49e120587c31
dlove13 <dlove13@ku.edu>
2026-03-07 01:38:53 -0600

    SE Lab modified

--- Software_Engineering_Labs/Lab_5/sales_report/sales.c
+++ Software_Engineering_Labs/Lab_5/sales_report/sales.c
@@ -1,6 +1,62 @@
-int main() {
+#include <stdio.h>
+#include "sales.h"
 
-return 0;
+//Function Definitions
+
+//Displays Month and Sales in order by month
+void Display(const char *month[], float  report[]) {
+        
+        printf("%s\n\n", "Monthly Sales Report for 2026");
+        printf("%-12s %s\n", "Month", "Sales");
+
+        for (int i = 0; i < 12; i++) {
+                printf("%-12s %.2f\n",month[i], report[i]);
+         }
+}
+
+//Displays Summary Containing min and max and average
+void Summary(const char *month[], float report[]) {}
+
+//Sorts the report from high to low
+void Sort(const char *month[], float report[]){}
+
+//Displays moving average over 6-months
+void MovingAverage(const char *month[], float report[]){}
+
+//Returns the max from the report array
+float FindMax(const char *month[], float report[])
+{
+        float max = report[0];          //Init max to the first
+        for (int i = 0; i < 12; i++)    //value in report
+        {
+                if 
+        }
 
 }
 
+//Returns the min from the report array
+float FindMin(const char *month, float report[]){}
+
+//Returns the avg from the report array
+float FindAvg(const char *month, float report[]){}
+
+
+//Main function
+int main() 
+//Months of the Year
+const *months[12] = {"January", "February", "March",
+                        "April", "May", "June",
+                        "July", "August", "September",
+                        "October","November", "December"};
+
+//Report values in order of month
+float report[12] = {23458.01, 40112.00, 56011.85,  
+        37820.88, 37904.67, 60200.22,
+        72400.31, 56210.89, 67230.84,  
+        68233.12, 80950.34, 95225.22}; 
+
+
+Display(months, report);
+
+return 0;
+}
--- Software_Engineering_Labs/Lab_5/sales_report/sales.h
+++ Software_Engineering_Labs/Lab_5/sales_report/sales.h
@@ -1,1 +1,9 @@
+//Function Declarations
+void Display(const char  *month[], float report[]);
+void Summary(const char *month[], float report[]);
+void Sort(const char month *month[], float report[]);
+void MovingAverage(const char  *month[], float report[]);
 
+float FindMax(const char *month, float report[]);
+float FindMin(const char *month, float report[]);
+float FindAvg(const char *month, float report[]);
