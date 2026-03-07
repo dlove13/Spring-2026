8dd0dd16b887c161d1812a9a5ed7502b250ead48
dlove13 <dlove13@ku.edu>
2026-03-07 02:32:38 -0600

    added changes to sales_report

--- Software_Engineering_Labs/Lab_5/sales_report/sales.c
+++ Software_Engineering_Labs/Lab_5/sales_report/sales.c
@@ -23,34 +23,55 @@ void Sort(const char *month[], float report[]){}
 //Displays moving average over 6-months
 void MovingAverage(const char *month[], float report[]){}
 
+
 //Returns the max from the report array
 float FindMax(const char *month[], float report[])
 {
-        float max = report[0];          //Init max to the first
+        float *max = report[0];          //Init max to the first
         for (int i = 0; i < 12; i++)    //value in report
         {
-                if 
+                if (report[i] < *max) {
+                
+                        *max = report[i];
+                }
         }
 
 }
 
 //Returns the min from the report array
 float FindMin(const char *month, float report[]){}
-
+        
+        float *min = report[0];          //Init min to the first
+        for (int i = 0; i < 12; i++)    //value in report
+        {
+                if (report[i] > *min) {
+                
+                        *min = report[i];
+                }
+        }
 //Returns the avg from the report array
-float FindAvg(const char *month, float report[]){}
+float FindAvg(const char *month, float report[], int length){
+        
+        float total = 0;
+
+        float avg = 0;
+
+        for (int i = 0; i < length; i++) {
+                total += report[i];        
+        }
+}
 
 
 //Main function
-int main() 
+int main() {
 //Months of the Year
-const *months[12] = {"January", "February", "March",
+const char *months[12] = {"January", "February", "March",
                         "April", "May", "June",
                         "July", "August", "September",
                         "October","November", "December"};
 
 //Report values in order of month
-float report[12] = {23458.01, 40112.00, 56011.85,  
+const float report[12] = {23458.01, 40112.00, 56011.85,  
         37820.88, 37904.67, 60200.22,
         72400.31, 56210.89, 67230.84,  
         68233.12, 80950.34, 95225.22}; 
--- Software_Engineering_Labs/Lab_5/sales_report/sales.h
+++ Software_Engineering_Labs/Lab_5/sales_report/sales.h
@@ -1,9 +1,9 @@
 //Function Declarations
 void Display(const char  *month[], float report[]);
 void Summary(const char *month[], float report[]);
-void Sort(const char month *month[], float report[]);
-void MovingAverage(const char  *month[], float report[]);
+void Sort(const char *month[], float report[]);
+void MovingAverage(const char  *month[], float report[], int length);
 
-float FindMax(const char *month, float report[]);
-float FindMin(const char *month, float report[]);
-float FindAvg(const char *month, float report[]);
+float FindMax(const char *month[], float report[]);
+float FindMin(const char *month[], float report[]);
+float FindAvg(const char *month[], float report[]);
